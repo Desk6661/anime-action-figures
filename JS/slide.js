8fd93a850cbd37ce50b0de3fa843f9db0d356ec3
Desk6661 <sadhudip212@gmail.com>
2025-02-10 00:20:54 +0530

    Add files via upload

---
+++ JS/slide.js
@@ -0,0 +1,63 @@
+let slide1 = document.getElementById("slide1");
+let slide2 = document.getElementById("slide2");
+let slide3 = document.getElementById("slide3");
+
+let slide1_1 = document.getElementById("slide1-1");
+let slide1_2 = document.getElementById("slide1-2");
+
+let slide2_1 = document.getElementById("slide2-1");
+let slide2_2 = document.getElementById("slide2-2");
+
+let slide3_1 = document.getElementById("slide3-1");
+let slide3_2 = document.getElementById("slide3-2");
+
+slide1.addEventListener("click", () => {
+    slide1_1.classList.add("slide-left");
+    slide1_2.classList.add("slide-right");
+    slide1_1.style.opacity = "1";
+    slide1_2.style.opacity = "1";
+
+    slide2_1.classList.remove("slide-left");
+    slide2_2.classList.remove("slide-right");
+    slide2_1.style.opacity = "0";
+    slide2_2.style.opacity = "0";
+
+    slide3_1.classList.remove("slide-left");
+    slide3_2.classList.remove("slide-right");
+    slide3_1.style.opacity = "0";
+    slide3_2.style.opacity = "0";
+});
+
+slide2.addEventListener("click", () => {
+    slide2_1.classList.add("slide-left");
+    slide2_2.classList.add("slide-right");
+    slide2_1.style.opacity = "1";
+    slide2_2.style.opacity = "1";
+
+    slide1_1.classList.remove("slide-left");
+    slide1_2.classList.remove("slide-right");
+    slide1_1.style.opacity = "0";
+    slide1_2.style.opacity = "0";
+
+    slide3_1.classList.remove("slide-left");
+    slide3_2.classList.remove("slide-right");
+    slide3_1.style.opacity = "0";
+    slide3_2.style.opacity = "0";
+});
+
+slide3.addEventListener("click", () => {
+    slide3_1.classList.add("slide-left");
+    slide3_2.classList.add("slide-right");
+    slide3_1.style.opacity = "1";
+    slide3_2.style.opacity = "1";
+
+    slide1_1.classList.remove("slide-left");
+    slide1_2.classList.remove("slide-right");
+    slide1_1.style.opacity = "0";
+    slide1_2.style.opacity = "0";
+
+    slide2_1.classList.remove("slide-left");
+    slide2_2.classList.remove("slide-right");
+    slide2_1.style.opacity = "0";
+    slide2_2.style.opacity = "0";
+});
